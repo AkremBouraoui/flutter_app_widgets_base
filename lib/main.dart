44567db29801d9211330db89f980e059cfdf9b42
Akrem Bouraoui <bouraoui.akrem.25@hotmail.com>
2025-11-13 18:31:31 -0500

    étape 4 : Découvrir le Stack

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,18 @@ class MainApp extends StatelessWidget {
                   style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
               ),
-              Text('This is my first application')
+              Text('This is my first application'),
+              Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Container(width: 200,height: 200,color:Colors.blue[100]),
+                  Icon(Icons.star, size: 100, color: Colors.amber),
+                  Text(
+                    "Superposé!",
+                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                  )
+                ],
+              )
             ],
         ),
       ),
